9fc4740fe109c9dbc456174dadad907756ae2bdb
Johanne <johanne_lewis@hotmail.com>
2019-08-13 20:18:36 +0100

    Structure of search page

--- index.php
+++ index.php
@@ -4,6 +4,14 @@
 	<title>Search Engine</title>
 </head>
 <body>
-
+	<div class="main-section">
+		<div class="logo-container">
+			<img src="">
+		</div>
+		<div class="search-container">
+			<input type="text" name="term" class="search-box">
+			<input type="submit" value="Search" class="search-button">
+		</div>
+	</div>
 </body>
 </html>
