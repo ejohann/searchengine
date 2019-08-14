2bb13a6463d2a68c19f0af927db7f3e5fd5ea2d3
Johanne <johanne_lewis@hotmail.com>
2019-08-14 19:49:31 +0100

    Function for converting relative links to absolute links

--- crawl.php
+++ crawl.php
@@ -1,6 +1,21 @@
 <?php 
 	 include("classes/DomDocumentParser.php");
 
+	 function createLink($src, $url){
+	 	$scheme = parse_url($url)["scheme"]; //http
+	 	$host = parse_url($url)["host"]; // www.sunsetcity.gd / link
+
+	 	if(substr($src, 0, 2) =="//"){
+	 		$src = $scheme . ":" . $src;
+	 	  }
+	 	else if(substr($src, 0, 1) =="/"){
+	 		$src = $scheme . "://" . $src;
+	 	  }
+
+	 	//echo "SRC: $src<br/>";
+	 	//echo "URL: $url<br/>";
+	   }
+
 	function followLinks($url){
 		$parser = new DomDocumentParser($url);
 
@@ -16,6 +31,7 @@
 				continue;
 			 }
 
+			createLink($href, $url); 
 			echo $href . "<br/>";	
 		  }
 	  }
