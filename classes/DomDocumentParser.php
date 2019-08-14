7345956f7bd2d7e154e189ed80f39e920d6801fd
Johanne <johanne_lewis@hotmail.com>
2019-08-14 15:23:02 +0100

    Create options array for visiting websites

--- classes/DomDocumentParser.php
+++ classes/DomDocumentParser.php
@@ -3,7 +3,14 @@
 	class DomDocumentParser {
 
 		public function __construct($url){
-			echo "URL: $url";
+			$options = array(
+				'http' => array(
+					'method' => "GET",
+					'header' => "User-Agent: searchEngineBot/0.1\n"
+				 )
+			);
+
+
 		  }
 
 	  }
