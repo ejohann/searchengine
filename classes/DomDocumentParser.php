edf64efd5311b9c7b6d9231048ec6577bfee1bfd
Johanne <johanne_lewis@hotmail.com>
2019-08-14 15:30:17 +0100

    Open a stream and get the contents

--- classes/DomDocumentParser.php
+++ classes/DomDocumentParser.php
@@ -8,9 +8,11 @@
 					'method' => "GET",
 					'header' => "User-Agent: searchEngineBot/0.1\n"
 				 )
-			);
-
-
+			  );
+			$context = stream_context_create($options);
+			
+			$document = new DomDocument();
+			$document->loadHTML(file_get_contents($url, false, $context));
 		  }
 
 	  }
