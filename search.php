ca278f2e8a11c6e1b2cf639663c50d486c5131e7
Johanne <johanne_lewis@hotmail.com>
2019-08-14 12:58:42 +0100

    Markup for search page

--- search.php
+++ search.php
@@ -4,3 +4,22 @@
 		echo $_GET["term"];
 	}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Search Engine</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header">
+			<div class="header-content">
+				<div class="logo-container">
+					<img src="assets/images/search_engine_logo.png">
+				</div>
+			</div>
+		</div>	
+	</div>
+</body>
+</html>
