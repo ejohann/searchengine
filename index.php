7030bd45b2e6f16a16e1a6e0f4bfcf57a27809ca
Johanne <johanne_lewis@hotmail.com>
2019-08-14 10:59:59 +0100

    Add a div with wrapper class around all page elements

--- index.php
+++ index.php
@@ -5,15 +5,17 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-	<div class="main-section">
-		<div class="logo-container">
-			<img src="">
-		</div>
-		<div class="search-container">
-			<form action="search.php" method="GET">
-				<input type="text" name="term" class="search-box">
-				<input type="submit" value="Search" class="search-button">
-			</form>
+	<div class="wrapper index-page">
+		<div class="main-section">
+			<div class="logo-container">
+				<img src="">
+			</div>
+			<div class="search-container">
+				<form action="search.php" method="GET">
+					<input type="text" name="term" class="search-box">
+					<input type="submit" value="Search" class="search-button">
+				</form>
+			</div>
 		</div>
 	</div>
 </body>
