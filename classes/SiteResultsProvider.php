36142ad8333780c3d045e0c8ab627e41431eae71
Johanne <johanne_lewis@hotmail.com>
2019-08-15 20:11:39 +0100

    Create function for return html containing results

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -19,7 +19,30 @@
 			$query->execute();
 			$row = $query->fetch(PDO::FETCH_ASSOC);
 			return $row["total"];
-		  }   
+		  }  
+
+		public function getResultsHtml($page, $pageSize, $term){
+				$query = $this->connection->prepare("SELECT count(*) as total 
+					FROM sites WHERE title LIKE :term 
+					OR url LIKE :term 
+					OR keywords LIKE :term 
+					or description LIKE :term 
+					ORDER BY clicks DESC
+					");
+			$searchTerm = "%" .$term. "%";
+			$query->bindParam(":term", $searchTerm);
+			$query->execute();
+
+			$resultsHtml = "<div class='siteResults'>";
+
+			while($row = $query->fetch(PDO::FETCH_ASSOC)){
+				$title = $row['title'];
+				echo "$title<br/>";
+			  }
+
+			$resultsHtml .= "</div>";
+		  }
+
 	  }
 
 ?>
