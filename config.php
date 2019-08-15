4dbb6df2282bdf82886f07903f73967653dba197
Johanne <johanne_lewis@hotmail.com>
2019-08-15 12:32:30 +0100

    Fix bug in code

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 
 	try{
 		$connection = new PDO("mysql:dbname=searchengine_db;host=localhost;port=3307", "root", "");
-		$connection->setAttribite(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 	  }
 	 catch(PDOException $error){
 	 	 echo "Connection failed: " . $error->getMessage();
