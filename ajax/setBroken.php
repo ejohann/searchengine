a380d3171a5e01d2266767ef4951813c52a026f3
Johanne <johanne_lewis@hotmail.com>
2019-08-16 16:11:42 +0100

    Fix bug in code

--- ajax/setBroken.php
+++ ajax/setBroken.php
@@ -2,7 +2,7 @@
 	include("../config.php");
 
 	if(isset($_POST["src"])){
-		$query = $connection->prepare("UPDATE images SET broken = WHERE image_url = :image_url");
+		$query = $connection->prepare("UPDATE images SET broken = 1 WHERE image_url = :image_url");
 		$query->bindParam(":image_url", $_POST["src"]);
 		$query->execute();
 	  }
