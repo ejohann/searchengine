4524bc4c9987e7789451251874f9006881009d5f
Johanne <johanne_lewis@hotmail.com>
2019-08-16 15:39:52 +0100

    Append class name to image a tag

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 function loadImage(src, className){
 	var image = $("<img>");
 	image.on("load", function(){
-
+		$("." + className + " a").append(image);
 	});
 
 	image.on("error", function(){
