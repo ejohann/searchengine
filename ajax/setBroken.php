8a3a22f44e50afce5f9b0a8ea70fd651aefcef80
Johanne <johanne_lewis@hotmail.com>
2019-08-16 16:05:53 +0100

    Query for updating broken link column in database

--- ajax/setBroken.php
+++ ajax/setBroken.php
@@ -1,5 +1,12 @@
 <?php
 	include("../config.php");
 
-	
+	if(isset($_POST["src"])){
+		$query = $connection->prepare("UPDATE images SET broken = WHERE image_url = :image_url");
+		$query->bindParam(":image_url", $_POST["src"]);
+		$query->execute();
+	  }
+	 else{
+	 	echo "No image in request";
+	  }
 ?>
